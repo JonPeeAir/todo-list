19deb4cb6680b75267825760c6dea844aa5153ae
John Pierre Mendoza <johnpierremendoza@gmail.com>
2022-01-02 15:12:51 +0800

    build: setup webpack with my usual configs

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[name].[hash][ext][query]',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+
+            {
+                test: /\.(svg|png|jpg|gif)$/i,
+                type: "asset/resource",
+            },
+
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+}
